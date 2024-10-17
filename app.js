d1b6fc5a32ba1c707f9dc16fc389f7346785b1e8
jozimmerman <joshua.zimmerman@lexialearning.com>
2024-10-17 16:17:45 -0500

    updated to meet structure

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 // Importing necessary modules
 
 const express = require("express");
-const { getRandomQuote } = require("./utils.js");
-const { PORT = 3000, BASE_PATH = "http://localhost" } = process.env;
+
+const { PORT = 3001, BASE_PATH = "http://localhost" } = process.env;
 const app = express();
 
-//const
 
-//functions
+// const
 
-//middleware
+// functions
 
-//routes
+// middleware
 
-//main 
+// routes
+
+// main
 
 app.listen(PORT, () => {
   console.log("Link to the server");
