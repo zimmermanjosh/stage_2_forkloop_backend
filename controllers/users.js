9f57e888032cf893c025a1a1cd9298703fac9852
jozimmerman <zimmerman.joshua@gmail.com>
2024-11-16 10:19:28 -0600

    update: controllers to match documentation , update project to get running and follow project docs

--- controllers/users.js
+++ controllers/users.js
@@ -4,6 +4,7 @@ const getUsers = (req, res) => {
   User.find({})
     .then((users) => res.status(200).send(users))
     .catch((err) => {
+      /* trunk-ignore(eslint/no-console) */
       console.error(err);
       return res.status(500).send({ message: err.message });
     });
@@ -14,6 +15,7 @@ const createUser = (req, res) => {
   User.create({ name, avatar })
     .then((user) => res.status(201).send(user))
     .catch((err) => {
+      /* trunk-ignore(eslint/no-console) */
       console.error(err);
       if (err.name === "ValidationError") {
         return res.status(400).send({ message: err.message });
@@ -28,6 +30,7 @@ const getUser = (req, res) => {
     .orFail()
     .then((user) => res.status(200).send(user))
     .catch((err) => {
+      /* trunk-ignore(eslint/no-console) */
       console.error(err);
       if (err.name === "DocumentNotFoundError") {
         return res.status(404).send({ message: err.message });
--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -3,7 +3,6 @@ const router = require("express").Router();
 const {
   createItem,
   getItems,
-  updateItem,
   deleteItem,
 } = require("../controllers/clothingItems");
 
@@ -12,8 +11,6 @@ const {
 router.post("/", createItem);
 // read
 router.get("/", getItems);
-// update
-router.put("/:itemId", updateItem);
 // delete
 router.delete("/:itemId", deleteItem);
 
