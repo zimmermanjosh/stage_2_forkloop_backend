40aba08ec0b7eed4f3cf9b9452aa9bffb2b1f819
joshua zimmerman <zimmerman.joshua@gmail.com>
2025-03-02 15:22:08 -0600

    fix updates

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/config");
 const { ERROR_CODES, ERROR_MESSAGES } = require("../utils/errors");
 
-module.exports = (req, res, next) => {
+const auth = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith("Bearer ")) {
@@ -27,3 +27,5 @@ module.exports = (req, res, next) => {
   next();
   return undefined; // Explicitly return undefined to satisfy ESLint
 };
+
+module.exports = auth;
--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  JWT_SECRET: process.env.JWT_SECRET || "your_secret_key_here",
+  JWT_SECRET: process.env.JWT_SECRET || "securePassword123",
 };
