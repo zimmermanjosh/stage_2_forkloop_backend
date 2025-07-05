3c6265eadd80fc5cee53613cfe24dd2e91de4d6f
joshua zimmerman <zimmerman.joshua@gmail.com>
2025-07-05 16:31:31 -0600

    updated: app
    adde: new logger

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const { errors } = require('celebrate');
 const mongoose = require("mongoose");
+const cors = require("cors");
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
 mongoose.set('strictQuery', false);
 
-const cors = require("cors");
 const routes = require("./routes");
 const { login, createUser } = require("./controllers/users");
 const { validateAuth, validateUserBody } = require("./middlewares/validator");
@@ -11,7 +14,7 @@ const app = express();
 const { PORT = 3001, BASE_PATH = "http://localhost" } = process.env;
 
 const { ERROR_CODES, ERROR_MESSAGES } = require("./utils/errors");
-const { errors } = require('celebrate');
+
 
 mongoose
     .connect("mongodb://127.0.0.1:27017/wtwr_db")
@@ -37,8 +40,13 @@ app.use((req, res, next) => {
 app.use("/users", routes.userRouter);
 app.use("/items", routes.clothingItem);
 
+app.use(requestLogger);
 
+// app.use(routes);
+
+app.use(errorLogger); // enabling the error logger
 app.use(errors());
+
 
 app.use((req, res) => {
     res.status(ERROR_CODES.NOT_FOUND).send({ message: ERROR_MESSAGES.NOT_FOUND });
