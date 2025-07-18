0de19f43a12868c09ed78c1eb9e64cf6874b0390
joshua zimmerman <zimmerman.joshua@gmail.com>
2025-07-18 18:47:31 -0500

    update: fixed shorthand formatting

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   JWT_SECRET: process.env.JWT_SECRET ,
   JWT_EXPIRATION_TIME: process.env.JWT_EXPIRATION_TIME,
-  DOMAIN_URL = 'https://testwtwr.jumpingcrab.com'
+  DOMAIN_URL : 'https://testwtwr.jumpingcrab.com'
 };
