b063161c7d6485abb4a480d932eddcc3e967a2f0
joshua zimmerman <zimmerman.joshua@gmail.com>
2025-07-18 14:28:50 -0500

    updated: removed backdoor passwords
    moved: secure password to .env file
    
    added: pass and day time in .env file and added to gitignore

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  JWT_SECRET: process.env.JWT_SECRET || "securePassword123",
-  JWT_EXPIRATION_TIME: this.JWT_EXPIRATION_TIME || "7d",
+  JWT_SECRET: process.env.JWT_SECRET ,
+  JWT_EXPIRATION_TIME: process.env.JWT_EXPIRATION_TIME,
 };
