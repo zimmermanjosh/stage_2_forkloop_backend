f124fcd9070f99a72d0f1b941cad3e6fd2ea8870
joshua zimmerman <zimmerman.joshua@gmail.com>
2025-07-28 18:27:59 -0500

    check feeback from instructor

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,19 +1,14 @@
 const errorHandler = (err, req, res, next) => {
-    if (err.statusCode) {
-        return res.status(err.statusCode).send({ message: err.message });
-    }
-    if (err.name === 'ValidationError') {
-        return res.status(400).send({ message: 'Invalid data passed' });
-    }
-    if (err.name === 'CastError') {
-        return res.status(400).send({ message: 'Invalid ID format' });
-    }
-    if (err.code === 11000) {
-        return res.status(409).send({ message: 'Email already exists' });
-    }
-    if (err.name === 'JsonWebTokenError' || err.name === 'TokenExpiredError') {
-        return res.status(401).send({ message: 'Unauthorized access' });
-    }
-    return res.status(500).send({ message: 'Internal server error' });
+
+  const { statusCode = 500, message } = err;
+  res
+    .status(statusCode)
+    .send({
+
+      message: statusCode === 500
+        ? 'An error occurred on the server'
+        : message
+    });
 };
+
 module.exports = errorHandler;
--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  JWT_SECRET:"securePassword123",
-  JWT_EXPIRATION_TIME:"7d",
-  // JWT_SECRET: process.env.JWT_SECRET ,
-  // JWT_EXPIRATION_TIME: process.env.JWT_EXPIRATION_TIME,
-  DOMAIN_URL : 'https://testwtwr.jumpingcrab.com'
+  JWT_SECRET: process.env.JWT_SECRET || "securePassword123",
+  JWT_EXPIRATION_TIME: process.env.JWT_EXPIRATION_TIME || "7d",
+  DOMAIN_URL: 'https://testwtwr.jumpingcrab.com'
 };
